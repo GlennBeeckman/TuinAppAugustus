3c527605265e96f42ee7963ff44b72f150e639a3
GlennBeeckman <GlennBeeckman@users.noreply.github.com>
2020-07-30 14:44:28 +0200

    added errorhandling tuin

--- src/app/tuin/tuin-data.service.ts
+++ src/app/tuin/tuin-data.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
 import {Tuin} from './tuin/tuin.model';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
-import { Observable } from 'rxjs';
-import { map, tap, delay } from 'rxjs/operators';
+import { Observable, of, throwError } from 'rxjs';
+import { map, tap, delay, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -14,11 +14,23 @@ export class TuinDataService {
 
   get tuinen$(): Observable<Tuin[]> {
     return this.http.get(`${environment.apiUrl}/tuinen/`).pipe(
+      catchError(this.handleError),
       tap(console.log),
       map(
         (list: any[]): Tuin[] => list.map(Tuin.fromJSON)
       )
     );
+  }
+
+  handleError(err: any): Observable<never> {
+    let errorMessage: string;
+    if (err instanceof HttpErrorResponse) {
+      errorMessage = `'${err.status} ${err.statusText}' when accessing '${err.url}'`;
+    } else {
+      errorMessage = `an unknown error occurred ${err}`;
+    }
+    console.error(err);
+    return throwError(errorMessage);
   }
 
   addNewTuin(tuin: Tuin) {
--- src/app/tuin/tuin-list/tuin-list.component.ts
+++ src/app/tuin/tuin-list/tuin-list.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {TuinDataService} from '../tuin-data.service';
 import {Tuin} from '../tuin/tuin.model';
-import { Subject, Observable } from 'rxjs';
+import { Subject, Observable, empty, EMPTY } from 'rxjs';
 import { distinctUntilChanged, debounceTime,
-  map, filter } from 'rxjs/operators';
+  map, filter, catchError } from 'rxjs/operators';
 
 @Component({
   selector: 'app-tuin-list',
@@ -14,7 +14,8 @@ export class TuinListComponent {
   
   public filterTuinNaam: string;
   public filterTuin$ = new Subject<string>();
-  private _fetchTuinen$: Observable<Tuin[]> = this._tuinDataService.tuinen$;
+  private _fetchTuinen$: Observable<Tuin[]>;
+  public errorMessage: string = '';
 
   constructor(private _tuinDataService: TuinDataService) {
     this.filterTuin$
@@ -38,6 +39,15 @@ export class TuinListComponent {
 
   addNewTuin(tuin){
     this._tuinDataService.addNewTuin(tuin);
+  }
+
+  ngOnInit(): void {
+    this._fetchTuinen$ = this._tuinDataService.tuinen$.pipe(
+      catchError(err => {
+        this.errorMessage = err;
+        return EMPTY;
+      })
+    )
   }
 
 

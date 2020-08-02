afe972776d3606705d6e67f035b359e870f4a517
GlennBeeckman <GlennBeeckman@users.noreply.github.com>
2020-08-02 15:31:45 +0200

    added luchtdruk model

---
+++ src/app/omgeving/luchtdruk/luchtdruk.model.ts
@@ -0,0 +1,24 @@
+interface LuchtdrukJson{
+    datum: string;
+    waarde: number;
+}
+export class Luchtdruk {
+    
+    constructor(
+        private _datum = new Date(),
+        private _waarde: number
+    ){}
+
+    get datum(): Date {
+        return this._datum;
+    }
+
+    get waarde(): number {
+        return this._waarde;
+    }
+
+    static fromJSON(json: LuchtdrukJson): Luchtdruk {
+        const foto = new Luchtdruk(new Date(json.datum), json.waarde);
+        return foto;
+    }
+}

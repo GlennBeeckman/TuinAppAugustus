43088ca49374f4a79117ffe736362ff2ef2b7844
GlennBeeckman <GlennBeeckman@users.noreply.github.com>
2020-08-02 15:32:48 +0200

    added temperatuur model

---
+++ src/app/omgeving/temperatuur/temperatuur.model.ts
@@ -0,0 +1,24 @@
+interface TemperatuurJson{
+    datum: string;
+    waarde: number;
+}
+export class Temperatuur {
+    
+    constructor(
+        private _datum = new Date(),
+        private _waarde: number
+    ){}
+
+    get datum(): Date {
+        return this._datum;
+    }
+
+    get waarde(): number {
+        return this._waarde;
+    }
+
+    static fromJSON(json: TemperatuurJson): Temperatuur {
+        const foto = new Temperatuur(new Date(json.datum), json.waarde);
+        return foto;
+    }
+}

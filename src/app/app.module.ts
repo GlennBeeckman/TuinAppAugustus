723a5bdbecda0bd516a9ed95d72254328e8137cc
GlennBeeckman <GlennBeeckman@users.noreply.github.com>
2020-08-05 20:33:19 +0200

    added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,16 @@ import { AppComponent } from './app.component';
 import { TuinModule } from './tuin/tuin.module';
 import { MaterialModule } from './material/material.module';
 import { OmgevingModule } from './omgeving/omgeving.module';
+import { RouterModule, Routes } from '@angular/router';
+import { TuinListComponent } from './tuin/tuin-list/tuin-list.component';
+import { OmgevingComponent } from './omgeving/omgeving/omgeving.component';
+import { OmgevingListComponent } from './omgeving/omgeving-list/omgeving-list.component';
+
+const appRoutes: Routes = [
+  { path: 'tuin/lijst', component: TuinListComponent },
+  { path: 'tuin/omgeving', component: OmgevingListComponent },
+  { path: '', redirectTo: 'tuin/lijst', pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
@@ -13,7 +23,8 @@ import { OmgevingModule } from './omgeving/omgeving.module';
     BrowserModule,
     TuinModule,
     MaterialModule,
-    OmgevingModule
+    OmgevingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
